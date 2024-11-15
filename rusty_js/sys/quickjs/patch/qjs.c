b589394883772a54be7df01a4be5c7f173a8c177
Michael <michael.zhu@sweet7.com>
2024-11-15 11:41:41 +0800

    fix: patch/qjs.c is not part of library

--- rusty_js/sys/quickjs/patch/qjs.c
+++ rusty_js/sys/quickjs/patch/qjs.c
@@ -1,22 +1,23 @@
 #include "quickjs.h"
 
+#include "qjs.h"
+
 // TODO: handle exeception
 JSValue QJS_RunScript(JSContext *ctx, char *script, int len){
     JSValue val;
-    JSContext *ctx1;
-    JSRuntime *rt;
-    int ret;
 
-    rt = JS_GetRuntime(ctx);
     val = JS_Eval(ctx, script, len, "script", 0);
     return val;
 }
 
 // TODO: handle exeception
-void QJS_RunJobs(JSRuntime *rt)
+void QJS_RunJobs(JSRuntime *rt){
+    int ret;
+    JSContext *ctx;
+
     for(;;) {
 
-        ret=JS_ExecutePendingJob(rt, &ctx1);
+        ret=JS_ExecutePendingJob(rt, &ctx);
         if (ret==0) break; // no job pending
 
         if (ret<0){
@@ -24,4 +25,4 @@ void QJS_RunJobs(JSRuntime *rt)
             break;
         }
     }
-)
+}
