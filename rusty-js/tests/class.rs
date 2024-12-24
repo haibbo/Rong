0c52c9189920d00840c6f7b87465680ae33c3e14
Michael <michael.zhu@sweet7.com>
2024-12-24 13:36:41 +0800

    test(class): add function and constructor test cases
    Signed-off-by: Michael <michael.zhu@sweet7.com>

--- rusty-js/tests/class.rs
+++ rusty-js/tests/class.rs
@@ -35,11 +35,32 @@ impl JSClass<JSEngineValue> for Point {
 }
 
 #[test]
-fn basic() {
+fn constructor() {
     run(|ctx| {
         ctx.register_class::<Point>();
-        let point = ctx.eval::<Point>("let t=new Point(2,3);t").unwrap();
+        let point = ctx.eval::<Point>("let point=new Point(2,3);point").unwrap();
         assert_eq!(point.x, 2);
         assert_eq!(point.y, 3);
+
+        assert_eq!(
+            ctx.eval::<String>("Point.constructor.name").unwrap(),
+            "Function"
+        );
+        assert_eq!(ctx.eval::<String>("typeof Point").unwrap(), "function");
+        assert!(ctx.eval::<bool>("point instanceof Point").unwrap());
+    });
+}
+
+#[test]
+fn function() {
+    run(|ctx| {
+        let func = ctx
+            .register_function(|a: i32, b: i32, c: i32| a + b + c)
+            .name("add");
+        ctx.global_object().set("add", func);
+
+        assert_eq!(ctx.eval::<i32>("add(7, 9,1)").unwrap(), 17);
+        assert_eq!(ctx.eval::<i32>("add.length").unwrap(), 3);
+        assert_eq!(ctx.eval::<String>("add.name").unwrap(), "add");
     });
 }
