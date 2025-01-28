c1b3aff96b42fb22da4a6ce80079c34bf3b6945c
Michael <michael.zhu@sweet7.com>
2025-01-28 23:12:11 +0800

    feat(macro): implement basic JSClass trait generation for methods macro
    Generates JSClass implementation with constructor support.
    The macro now correctly processes #[method(constructor)] attribute and
    generates the corresponding data_constructor() method.

--- rusty-js/macro/src/lib.rs
+++ rusty-js/macro/src/lib.rs
@@ -1,7 +1,9 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemStruct};
+use quote::quote;
+use syn::{parse_macro_input, ItemImpl, ItemStruct};
 
 mod class;
+mod methods;
 
 /// Example:
 /// Attribute macro for creating JavaScript classes from Rust structs
@@ -32,4 +34,50 @@ pub fn class(_attr: TokenStream, item: TokenStream) -> TokenStream {
         Ok(expanded) => expanded.into(),
         Err(err) => err.to_compile_error().into(),
     }
+}
+
+/// Attribute macro for method configuration
+#[proc_macro_attribute]
+pub fn method(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Just pass through the original item
+    item
+}
+
+/// Attribute macro for implementing methods
+#[proc_macro_attribute]
+pub fn methods(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemImpl);
+    let methods: Vec<_> = input
+        .items
+        .iter()
+        .filter_map(|item| {
+            if let syn::ImplItem::Fn(method) = item {
+                // Only include methods that have our #[method] attribute
+                if method
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("method"))
+                {
+                    Some(method.clone())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let impl_tokens = match methods::methods_impl(&input, &methods) {
+        Ok(tokens) => tokens,
+        Err(err) => return TokenStream::from(err.to_compile_error()),
+    };
+
+    let expanded = quote! {
+        #input
+
+        #impl_tokens
+    };
+
+    TokenStream::from(expanded)
 }
---
+++ rusty-js/macro/src/methods.rs
@@ -0,0 +1,51 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{ImplItemFn, ItemImpl, Meta};
+
+/// Process method attributes and generate JavaScript bindings
+pub fn methods_impl(input: &ItemImpl, methods: &[ImplItemFn]) -> syn::Result<TokenStream> {
+    let impl_type = &input.self_ty;
+
+    // Find constructor method
+    let constructor = methods
+        .iter()
+        .find(|method| {
+            method.attrs.iter().any(|attr| {
+                attr.path().is_ident("method")
+                    && attr
+                        .meta
+                        .require_list()
+                        .ok()
+                        .and_then(|list| list.parse_args::<Meta>().ok())
+                        .map_or(false, |meta| meta.path().is_ident("constructor"))
+            })
+        })
+        .map(|method| {
+            let method_name = &method.sig.ident;
+            quote! {
+                fn data_constructor() -> rusty_js::function::Constructor<rusty_js::JSEngineValue> {
+                    rusty_js::function::Constructor::new(Self::#method_name)
+                }
+            }
+        })
+        .unwrap_or_else(|| {
+            quote! {
+                fn data_constructor() -> rusty_js::function::Constructor<rusty_js::JSEngineValue> {
+                    rusty_js::function::Constructor::new(|_: ()| panic!("No constructor defined"))
+                }
+            }
+        });
+
+    let output = quote! {
+        impl rusty_js::JSClass<rusty_js::JSEngineValue> for #impl_type {
+            const NAME: &'static str = stringify!(#impl_type);
+
+            #constructor
+
+            fn class_setup(_class: &rusty_js::ClassSetup<rusty_js::JSEngineValue>) {}
+        }
+    };
+
+    // println!("Generated code:\n{}", output.to_string());
+    Ok(output)
+}
