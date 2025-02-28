57e54acb2a033075e287184944ec47f201d7b3f8
Michael <michael.zhu@sweet7.com>
2025-02-28 23:55:14 +0800

    feat(core): bridge rust's Stream to JS asyncIterable
    Signed-off-by: Michael <michael.zhu@sweet7.com>

--- rusty-js/core/src/iterator.rs
+++ rusty-js/core/src/iterator.rs
@@ -2,6 +2,7 @@ use crate::{
     IntoJSValue, JSContext, JSExceptionHandler, JSFunc, JSObject, JSObjectOps, JSResult, JSSymbol,
     JSValue,
 };
+use futures::{Stream, StreamExt};
 
 pub trait ToJSIterator<T>
 where
@@ -63,5 +64,61 @@ where
     I: IntoIterator<Item = T>,
     I: Clone,
     I: 'static,
+{
+}
+
+pub trait ToJSAsyncIterator<T>
+where
+    Self: Stream<Item = T> + 'static + Sized + Clone,
+{
+    fn to_js_async_iter<V>(&self, ctx: &JSContext<V::Context>) -> JSResult<JSObject<V>>
+    where
+        T: IntoJSValue<V>,
+        V: JSObjectOps + 'static,
+        V::Context: JSExceptionHandler,
+    {
+        let iterable = JSObject::new(ctx);
+        let target = self.clone();
+
+        let iterator = JSFunc::new(ctx, move |ctx: JSContext<V::Context>| {
+            let obj = JSObject::new(&ctx);
+            let mut stream = Box::pin(target.clone());
+
+            let next = JSFunc::new(&ctx, async |ctx: JSContext<V::Context>| {
+                let result = JSObject::new(&ctx);
+
+                match stream.next().await {
+                    Some(i) => {
+                        let _ = result.set("done", false);
+                        let value = i.into_js_value(&ctx);
+                        let value = JSValue::from_raw(&ctx, value);
+                        result.set("value", value)?;
+                    }
+                    None => {
+                        result.set("done", true)?;
+                    }
+                }
+                Ok(result)
+            })?;
+
+            obj.set("next", next)?;
+            Ok(obj)
+        })?;
+
+        let constant = ctx
+            .global()
+            .get::<_, JSObject<V>>("Symbol")?
+            .get::<_, JSSymbol<V>>("asyncIterator")?;
+        iterable.set(constant, iterator)?;
+        Ok(iterable)
+    }
+}
+
+// Blanket implementation for all Stream types
+impl<T, S> ToJSAsyncIterator<T> for S
+where
+    S: Stream<Item = T>,
+    S: Clone,
+    S: 'static,
 {
 }
